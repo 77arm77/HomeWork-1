66e528e115a7ae523c3f23e4390a798da0ec8ed2
avto <aram.kirakosyan.2001@mail.ru>
2022-04-26 00:07:45 +0400

    Initial Commit

---
+++ HomeWork-1/main.swift
@@ -0,0 +1,120 @@
+//
+//  main.swift
+//  HomeWork-1
+//
+//  Created by GITC 11 on 26.04.22.
+//
+
+import Foundation
+
+1.1
+var a: Int = 100
+var b: Int = 399
+var c: Double = 3.55
+var d: Double = -5.66
+var e: Int = 1
+var f: Int = 0
+var g: Int = -1
+var life: String = "string"
+var life1: String = "hello"
+print("\(a),\(b),\(c),\(d) , \(e),\(f), \(g) ,\(life),\(life1)")
+
+
+1.2
+let a: Int = 100
+let b: Int = 399
+let c: Double = 3.55
+let d: Double = -5.66
+let e: Int = 1
+let f: Int = 0
+let g: Int = -1
+let relax : String = "String"
+let relaxx: String = "Hello"
+print("\(a),\(b),\(c),\(d) , \(e),\(f), \(g) ,\(relax),\(relaxx)")
+
+
+
+//1.3
+let a: Int = 10
+let b: Int = 399
+let result = a + b
+print("result = \(result)")
+
+
+//1.4
+let a: Int = 13
+let b: Int = 459
+let result = a - b
+print("result = \(result)")
+
+
+//1.5
+let a: Int = 13
+let b: Int = 459
+let result = a * b
+print("result = \(result)")
+
+
+
+1.6
+let a: Int = 400
+let b: Int = 3
+let result = a / b
+print("result = \(result)")
+
+
+
+1.7
+let a: Int = 456
+let b: Int = 3
+let result = a % b
+print("result = \(result)")
+
+
+//1.8
+var a: Int = 100
+let b: Int = 300
+a += b
+print("a = \(a)")
+
+
+//1.9
+var a: Int = 100
+let b: Int = 300
+a *= b
+print("a = \(a)")
+
+
+//
+1.10
+var a: Int = 100
+let b: Int = 300
+a -= b
+print("a = \(a)")
+
+
+
+//1.11
+var a: Int = 300
+let b: Int = 100
+ a /= b
+print("a = \(a)")
+
+
+//1.12
+var a: Int = 300
+let b: Int = 100
+ a = b
+print("a = \(a)")
+//
+
+
+1.13
+let a: Int = 13
+let b: Int = 459
+let isEqual = a == b
+let isLess = a < b
+let isGreater = a > b
+let isNotEqual = a != b
+print("isEqual = \(isEqual),isLess = \(isLess), isGreater = \(isGreater), isNotEqual = \(isNotEqual)  ")
+
